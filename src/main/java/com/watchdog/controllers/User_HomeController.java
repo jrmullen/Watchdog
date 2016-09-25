69be2a66b89ef219159f296f73e071c4ff46eea9
Renee <rlwise@oakland.edu>
2016-09-25 01:39:34 -0400

    Revert "fixed accidental move of files"
    This reverts commit 83d789520300b6a0d30c1729b0ee1a4a6191f15c.

--- src/main/java/com/watchdog/controllers/User_HomeController.java
+++ src/main/java/com/watchdog/controllers/User_HomeController.java
@@ -1,24 +1,15 @@
 package com.watchdog.controllers;
 
-        import org.springframework.stereotype.Controller;
-        import org.springframework.ui.Model;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RequestMethod;
-        import org.springframework.web.bind.annotation.RequestParam;
-        import java.security.Principal;
-
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class User_HomeController {
 
     @RequestMapping("/user_home")
     public void userHome(){
-    }
-
-    @RequestMapping(value = "/user_home", method = RequestMethod.GET)
-    public String index(Principal principal) {
-        return principal != null ? "user_home" : "user_home";
     }
 
 }
