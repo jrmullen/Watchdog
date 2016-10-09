10566b228b684baf0ee9d9fafcae61155cbb5344
Jeremy <jrmullen@oakland.edu>
2016-10-08 22:17:40 -0400

    Functional logout button added to header

---
+++ src/main/java/ThymeleafConfig.java
@@ -0,0 +1,16 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+
+/**
+ * Created by jmullen on 10/8/16.
+ */
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringSecurityDialect springSecurityDialect() {
+        return new SpringSecurityDialect();
+    }
+}
