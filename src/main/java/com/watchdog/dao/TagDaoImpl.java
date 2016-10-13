3e1f45b20330f3413d42a67b497a26311281d59a
jdtaormi <jdtaormi@gmail.com>
2016-10-13 12:39:49 -0400

    tagdao creation

---
+++ src/main/java/com/watchdog/dao/TagDaoImpl.java
@@ -0,0 +1,33 @@
+package com.watchdog.dao;
+
+import com.watchdog.business.Tag;
+
+import java.util.List;
+
+public class TagDaoImpl implements TagDao {
+
+    //Create
+    public void save(Tag tag){
+
+    }
+
+    //Read
+    public Tag getById(int id){
+        
+    }
+
+    //Update
+    public void update(Tag tag){
+
+    }
+
+    //Delete
+    public void deleteById(int id){
+
+    }
+
+    //Get All
+    public List<Tag> getAll(){
+
+    }
+}
