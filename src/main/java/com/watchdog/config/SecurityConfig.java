043e5d7bcba369818689c5f8af2fe3d4998ef93a
Briana Buckner <BBuck@TheLioness.local>
2016-10-23 19:18:09 -0400

    updated SecurityConfig.java to include making device_manager page restricted without logging in

--- src/main/java/com/watchdog/config/SecurityConfig.java
+++ src/main/java/com/watchdog/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/user_home").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+                .antMatchers("/device_manager").access("hasRole('ROLE_ADMIN')")
                 .and()
                 .formLogin().failureUrl("/login?error")
                 .defaultSuccessUrl("/user_home")
