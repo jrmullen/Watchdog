3b212bf85b67bcae25e4ddde9b00a0d70559d0ab
Jeremy <jrmullen@oakland.edu>
2016-11-01 10:39:37 -0400

    Refactored variable names for Tag class

--- src/main/java/com/watchdog/business/Tag.java
+++ src/main/java/com/watchdog/business/Tag.java
@@ -1,31 +1,31 @@
 package com.watchdog.business;
 
 public class Tag {
-    private int tag_id;
-    private int vid_id;
-    private String tag_name;
+    private int tagId;
+    private int vidId;
+    private String tagName;
 
-    public int getTag_id() {
-        return tag_id;
+    public int getTagId() {
+        return tagId;
     }
 
-    public void setTag_id(int tag_id) {
-        this.tag_id = tag_id;
+    public void setTagId(int tagId) {
+        this.tagId = tagId;
     }
 
-    public int getVid_id() {
-        return vid_id;
+    public int getVidId() {
+        return vidId;
     }
 
-    public void setVid_id(int vid_id) {
-        this.vid_id = vid_id;
+    public void setVidId(int vidId) {
+        this.vidId = vidId;
     }
 
-    public String getTag_name() {
-        return tag_name;
+    public String getTagName() {
+        return tagName;
     }
 
-    public void setTag_name(String tag_name) {
-        this.tag_name = tag_name;
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
     }
 }
---
+++ src/test/java/com/watchdog/VideoTests.java
@@ -0,0 +1,35 @@
+package com.watchdog;
+
+import com.watchdog.business.User;
+import com.watchdog.business.Video;
+import com.watchdog.dao.UserDao;
+import com.watchdog.dao.VideoDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by Jeremy on 11/1/2016.
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class VideoTests {
+
+    @Mock
+    private Video video;
+    @Mock
+    private VideoDao videoDao;
+
+    @Test
+    public void testMockCreation() {
+        assertNotNull(video);
+        assertNotNull(videoDao);
+    }
+}
