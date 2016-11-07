4f02b0ac2ea4edf1d1b8768e3051726c41d6ddd0
Jeremy Mullen <jmullen@ip-10-136-169-244.us-west-2.compute.internal>
2016-11-07 13:39:49 -0500

    changed port to SMTP msa protocol email port

--- src/main/java/com/watchdog/services/PasswordResetService.java
+++ src/main/java/com/watchdog/services/PasswordResetService.java
@@ -51,7 +51,7 @@ public class PasswordResetService {
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "25");
+        props.put("mail.smtp.port", "587");
 
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
