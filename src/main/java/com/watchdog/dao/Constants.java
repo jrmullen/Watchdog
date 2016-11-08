fe98699fa1a05e4d4c9202c009b9f1f4da93915c
jdtaormi <jdtaormi@gmail.com>
2016-11-07 22:15:50 -0500

    CONSTANTS

--- src/main/java/com/watchdog/dao/Constants.java
+++ src/main/java/com/watchdog/dao/Constants.java
@@ -25,11 +25,6 @@ public class Constants {
 
 
     // Video queries
-    public static final String UPDATE_VIDEO_BY_VIDEO_ID_QUERY = "update video set VID_FILE_PATH = ?, VID_LENGTH = ?, VID_IS_COMPRESSED = ?, VID_IS_ENCRYPTED = ?, VID_SIZE_ON_DISK = ?, VID_DATE = ?, VID_TIME = ?, VID_TITLE = ?, VID_LOCATION = ?, VID_DESCRIPTION = ?, DEVICE_MAC = ? where VID_ID = ?";
-    public static final String DELETE_VIDEO_BY_VIDEO_ID_QUERY = "delete from video where VID_ID=?";
-    public static final String GET_ALL_VIDEOS_QUERY = "select VID_ID, USER_ID, VID_FILE_PATH, VID_LENGTH, VID_IS_COMPRESSED, VID_IS_ENCRYPTED, VID_SIZE_ON_DISK, VID_DATE, VID_TIME, VID_TITLE, VID_LOCATION, VID_DESCRIPTION, DEVICE_MAC from video";
-
-
     public static final String CREATE_VIDEO_QUERY = "insert into video (USER_ID, VID_FILE_PATH, VID_LENGTH, VID_IS_COMPRESSED, VID_IS_ENCRYPTED, VID_SIZE_ON_DISK, VID_DATE, VID_TIME, VID_TITLE, VID_LOCATION, VID_DESCRIPTION, DEVICE_MAC) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     public static final String GET_VIDEO_BY_VIDEO_ID = "select USER_ID, VID_FILE_PATH, VID_LENGTH, VID_IS_COMPRESSED, VID_IS_ENCRYPTED, VID_SIZE_ON_DISK, VID_DATE, VID_TIME, VID_TITLE, VID_LOCATION, VID_DESCRIPTION, DEVICE_MAC from video where VID_ID = ?";
     public static final String GET_VIDEO_TITLE_BY_USER_ID = "select VID_TITLE from video where USER_ID = ?";
@@ -46,4 +41,10 @@ public class Constants {
     public static final String GET_TAG_BY_VIDEO_ID = "select TAG_ID, VID_ID, TAG_NAME from tag where VID_ID = ?";
     public static final String DELETE_TAG_BY_VID_ID_QUERY = "delete from tag where VID_ID=?";
 
+    // Permissions
+    public static final String CREATE_PERMISSIONS_QUERY = "insert into permissions (ROLE, PERMISS_DESCRIPTION) values (?,?)";
+    public static final String GET_BY_PERMISSIONS_ID_QUERY = "select PERMISS_ID, ROLE, PERMISS_DESCRIPTION from permissions where PERMISS_ID = ?";
+    public static final String UPDATE_BY_PERMISSIONS_ID_QUERY = "update permissions set ROLE = ?, PERMISS_DESCRIPTION = ? from permissions where PERMISS_ID = ?";
+    public static final String DELETE_PERMISSIONS_BY_ID_QUERY = "delete from permissions where PERMISS_ID = ?";
+    public static final String GET_ALL_PERMISSIONS_QUERY = "select PERMISS_ID, ROLE, PERMISS_DESCRIPTION from permissions";
 }
