d80a33a56fcfa689eede5a8747a8d03ab08df971
Jeremy <jrmullen@oakland.edu>
2016-11-07 22:34:23 -0500

    Refactored duplicate code

--- src/main/java/com/watchdog/controllers/DeviceManagerController.java
+++ src/main/java/com/watchdog/controllers/DeviceManagerController.java
@@ -11,15 +11,15 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping(value = "/device_manager", method = { RequestMethod.GET, RequestMethod.POST })
+@RequestMapping(value = "/device_manager", method = {RequestMethod.GET, RequestMethod.POST})
 public class DeviceManagerController {
+
+    //Initialize database and create DeviceDao object
+    ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+    DeviceDao deviceDao = ctx.getBean("deviceDaoImpl", DeviceDao.class); //first parameter is the id found in the spring.xml file
 
     @RequestMapping
     public String listAll(@Valid Device device, BindingResult bindingResult, Model model) {
-
-        //Initialize database and create DeviceDao object
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
-        DeviceDao deviceDao = ctx.getBean("deviceDaoImpl", DeviceDao.class); //first parameter is the id found in the spring.xml file
 
         model.addAttribute("deviceList", deviceDao.getAll());
 
@@ -30,10 +30,6 @@ public class DeviceManagerController {
     @PostMapping(params = "addDevice")
     public String addNew(@Valid Device device, Model model) {
 
-        //Initialize database and create DeviceDao object
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
-        DeviceDao deviceDao = ctx.getBean("deviceDaoImpl", DeviceDao.class); //first parameter is the id found in the spring.xml file
-
         String errorMessage = null;
         String successMessage = null;
 
@@ -42,33 +38,30 @@ public class DeviceManagerController {
         model.addAttribute("deviceIp", device.getDeviceAddress());
 
         //Save device to DB
-        if(!device.getDeviceName().equals("") && !device.getDeviceMac().equals("")
+        if (!device.getDeviceName().equals("") && !device.getDeviceMac().equals("")
                 && !device.getDeviceAddress().equals("")) {
 
             String testNumeric = "";
             model.addAttribute("devicePort", testNumeric);
 
-           /* if(isNumeric(testNumeric)) {
-                model.addAttribute("devicePort", device.getDevicePort());*/
+//            if(isNumeric(testNumeric)) {
+//                model.addAttribute("devicePort", device.getDevicePort());
 
-                if (deviceDao.checkMacExists(device.getDeviceMac())) {
-                    model.addAttribute("errorMessage", "A device with this MAC address already exists." +
-                            " Please enter a unqiue MAC address.");
-                }
-                else if (device.getDevicePort() < 1 | device.getDevicePort() > 65535) {
-                    model.addAttribute("errorMessage", "Port field must be left blank or be number between 1 and 65,535.");
-                }
-                else {
-                    deviceDao.save(device);
-                    model.addAttribute("successMessage", "Device successfully saved.");
-                }
+            if (deviceDao.checkMacExists(device.getDeviceMac())) {
+                model.addAttribute("errorMessage", "A device with this MAC address already exists." +
+                        " Please enter a unqiue MAC address.");
+            } else if (device.getDevicePort() < 1 | device.getDevicePort() > 65535) {
+                model.addAttribute("errorMessage", "Port field must be left blank or be number between 1 and 65,535.");
+            } else {
+                deviceDao.save(device);
+                model.addAttribute("successMessage", "Device successfully saved.");
+            }
           /*  }
             else {
                 model.addAttribute("errorMessage", "Port field must be a number.");
             }*/
 
-        }
-        else if (device.getDeviceName().equals("") || device.getDeviceMac().equals("")
+        } else if (device.getDeviceName().equals("") || device.getDeviceMac().equals("")
                 || device.getDeviceAddress().equals("")) {
             model.addAttribute("errorMessage", "Please fill in all required fields.");
 
@@ -85,11 +78,7 @@ public class DeviceManagerController {
 
     //delete device
     @RequestMapping(params = "deleteDevice")
-    public String delete (@RequestParam int id, Device device, Model model ) {
-
-        //Initialize database and create DeviceDao object
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
-        DeviceDao deviceDao = ctx.getBean("deviceDaoImpl", DeviceDao.class); //first parameter is the id found in the spring.xml file
+    public String delete(@RequestParam int id, Device device, Model model) {
 
         deviceDao.deleteById(id);
         model.addAttribute("deviceList", deviceDao.getAll());
@@ -97,8 +86,7 @@ public class DeviceManagerController {
         return "/device_manager";
     }
 
-    public static boolean isNumeric(String str)
-    {
+    public static boolean isNumeric(String str) {
         return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
     }
 }
