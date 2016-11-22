875f15e048dbb559766708c65d9ce20116ab82cd
Renee <rlwise@oakland.edu>
2016-11-21 21:29:16 -0500

    Threading working. :)

--- src/main/java/com/watchdog/Application.java
+++ src/main/java/com/watchdog/Application.java
@@ -9,18 +9,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.sql.Date;
-import java.sql.Time;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class Application {
 
     private static File directory = new File("c:/ftp/video");
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(Application.class, args);
 
         //Get the Spring Context
@@ -29,6 +28,46 @@ public class Application {
         //To use JdbcTemplate
         UserDao userDao = ctx.getBean("userDaoImpl", UserDao.class); //first parameter is the id found in the spring.xml file
         VideoDao videoDao = ctx.getBean("videoDaoImpl", VideoDao.class);
+
+        // Run thread to check for add or delete video files and add the video info to the database
+        ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+        exec.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("\n\nRUNNING THREAD");
+                if (directory.exists()) {
+
+                    VideoInsertDeleteService videoInsertDeleteService = new VideoInsertDeleteService();
+                    List<File> fileList = videoInsertDeleteService.getFiles(directory);
+
+                    // delete file if its info exists in database but not in folder
+                    // steps
+                    // 1. check if exists in db
+                    // 2. if exists in db, check if exists in folder
+                    // 3. if exists in db and NOt in folder, delete video info from db
+                    // 4. otherwise do nothing
+
+                    //if(videoDao.getByVideoTitle())
+
+                    for (final File file : fileList) {
+                        if (videoInsertDeleteService.overMaxAllowedAge(file)) {
+                            System.out.println("Delete file:  " + file.getName());
+
+                            videoInsertDeleteService.deleteFile(file);
+
+                        }
+                        else if (videoInsertDeleteService.fileExists(file.getName(), directory) &&
+                                !videoDao.checkVideoExists(file.getName())) {
+                            videoInsertDeleteService.insertVideoIntoDb(file, directory);
+                        }
+                    }
+                }
+                else {
+                    System.out.println("Error! Unable to locate directory: " + directory.toString());
+                }
+
+            }
+        }, 0, 60, TimeUnit.SECONDS);
 
         /*// start test video query code here
 
@@ -102,41 +141,7 @@ public class Application {
 
         // end test video query code here
 
-
-     if (directory.exists()) {
-         VideoInsertDeleteService videoInsertDeleteService = new VideoInsertDeleteService();
-         List<File> fileList = videoInsertDeleteService.getFiles(directory);
-
-         // delete file if its info exists in database but not in folder
-         // steps
-         // 1. check if exists in db
-         // 2. if exists in db, check if exists in folder
-         // 3. if exists in db and NOt in folder, delete video info from db
-         // 4. otherwise do nothing
-
-         //if(videoDao.getByVideoTitle())
-
-         for (final File file : fileList) {
-             if (videoInsertDeleteService.overMaxAllowedAge(file)) {
-                 System.out.println("Delete file:  " + file.getName());
-
-                 videoInsertDeleteService.deleteFile(file);
-
-             }
-             else if (videoInsertDeleteService.fileExists(file.getName(), directory) &&
-                     !videoDao.checkVideoExists(file.getName())) {
-                 videoInsertDeleteService.insertVideoIntoDb(file, directory);
-             }
-         }
-     }
-     else {
-         System.out.println("Error! Unable to locate directory: " + directory.toString());
-     }
         ctx.close();
     }
-
-
-
-
 
 }
